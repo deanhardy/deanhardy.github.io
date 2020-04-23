9dbde67082a98332b032d7edc57cfb223323ae39
“Dean <deanhardy@gmail.com>
2020-04-22 22:09:47 -0400

    let's see if this works

---
+++ index_files/cbgapi.js
@@ -0,0 +1,5 @@
+/* JS */ gapi.loaded_1(function(_){var window=this;
+_.z("gapi.rpc.setup",_.yl);_.z("gapi.rpc.register",_.nl);_.z("gapi.rpc.call",_.rl);_.z("gapi.rpc.unregister",_.ol);_.z("gapi.rpc.sendHandshake",function(a,b){_.yl(a)();_.il.send(a,_.il.PC(b),"*")});
+
+});
+// Google Inc.
